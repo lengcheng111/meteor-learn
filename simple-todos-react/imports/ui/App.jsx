c9e8aaff978d4b50177a80b2ffcd695bf864e45e
truong.le <truong.le@pythagoras.se>
2024-04-03 14:22:23 +0700

    version 1

--- simple-todos-react/imports/ui/App.jsx
+++ simple-todos-react/imports/ui/App.jsx
@@ -1,19 +1,63 @@
-import React from 'react';
-import {Task} from "./Task.jsx";
-import {TasksCollection} from "../api/TasksCollection.js";
+import { Meteor } from 'meteor/meteor';
+import React, { useState, Fragment } from 'react';
 import { useTracker } from 'meteor/react-meteor-data';
-import {TaskForm} from "./TaskForm.jsx";
+import { Task } from './Task';
+import { TaskForm } from './TaskForm';
+import { LoginForm } from './LoginForm';
+import {TasksCollection} from "../api/TasksCollection.js";
+
 
 export const App = () => {
-    const tasks = useTracker(() => TasksCollection.find({}, { sort: { createdAt: -1 }}).fetch());
+    const [hideCompleted, setHideCompleted] = useState(false);
+
+    const user = useTracker(() => Meteor.user());
+
+    const hideCompletedFilter = { isChecked: { $ne: true } };
+
+    const tasks = useTracker(() =>
+        TasksCollection.find(hideCompleted ? hideCompletedFilter : {}, {
+            sort: { createdAt: -1 },
+        }).fetch()
+    );
+
+    const toggleChecked = ({ _id, isChecked }) => {
+        TasksCollection.update(_id, {
+            $set: {
+                isChecked: !isChecked
+            }
+        })
+    };
+    const deleteTask = ({ _id }) => TasksCollection.remove(_id);
 
     return (
+        <div className="app">
+            <header>
+                <div className="app-bar">
+                    <div className="app-header">
+                        <h1>Welcome to Meteor!</h1>
+                    </div>
+                </div>
+            </header>
 
-        <div>
-            <TaskForm/>
-            <ul>
-                { tasks.map(task => <Task key={ task._id } task={ task }/>) }
-            </ul>
+            <div className="main">
+                <TaskForm/>
+                <div className="filter">
+                    <button onClick={() => setHideCompleted(!hideCompleted)}>
+                        {hideCompleted ? 'Show All' : 'Hide Completed'}
+                    </button>
+                </div>
+
+                <ul className="tasks">
+                    {tasks.map(task => (
+                        <Task
+                            key={task._id}
+                            task={task}
+                            onCheckboxClick={toggleChecked}
+                            onDeleteClick={deleteTask}
+                        />
+                    ))}
+                </ul>
+            </div>
         </div>
     );
 };
--- simple-todos-react/server/main.js
+++ simple-todos-react/server/main.js
@@ -1,9 +1,21 @@
 import { Meteor } from 'meteor/meteor';
 import {TasksCollection} from "../imports/api/TasksCollection.js";
+import { Accounts } from 'meteor/accounts-base';
+
+
+const SEED_USERNAME = 'meteorite';
+const SEED_PASSWORD = 'password';
 
 const insertTask = (taskText) => TasksCollection.insert({text: taskText});
 
 Meteor.startup(() => {
+  if (!Accounts.findUserByUsername(SEED_USERNAME)) {
+    Accounts.createUser({
+      username: SEED_USERNAME,
+      password: SEED_PASSWORD,
+    });
+  }
+
   if (TasksCollection.find().count() === 0) {
     [
       'First Task',
